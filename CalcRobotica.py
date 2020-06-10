700e2c4d73ef59037c854df33044edfd5a092d89
Magh <emgicraft.mastercmds@gmail.com>
2020-06-09 22:13:41 -0500

    Integracion completa de conversiones, aun por probar

--- CalcRobotica.py
+++ CalcRobotica.py
@@ -42,15 +42,13 @@ Ejemplos:
 #Lo más simple es ya tener la matriz resultante con toda la formula para cada elemento y simplemente reemplazar.
 calcop=str(input("Escriba operación: "))
 
+x = y = z = rc = re = tetaC = tetaE = fi = 0.0
+
 if calcop.startswith("Cart",0,4) or calcop.startswith("cart",0,4):
     calcop=calcop.replace("Cart ","")
     calcop=calcop.replace("cart ","")
 
     cart=calcop.split(" ")
-
-    x = y = z = rc = re = tetaC = tetaE = fi = 0.0
-
-    #elec=int(input("1-Cilindrica  2-Esféricas  Qué elije? "))
 
     x, y, z = float(cart[0]), float(cart[1]), float(cart[2])
 
@@ -78,8 +76,76 @@ r=%s
 teta=%s
 fi=%s
         """%(rc,tetaC,z,re,tetaE,fi))
+        
     else:
         print("Error, no ingreso tres coordenadas.")
+elif calcop.startswith("Cil",0,3) or calcop.startswith("cil",0,3):
+    calcop=calcop.replace("Cil ","")
+    calcop=calcop.replace("cil ","")
+
+    cil=calcop.split(" ")
+
+    rc, tetaC, z = float(cil[0]), float(cil[1]), float(cil[2])
+
+    if len(cil)==3:
+        #Calculo coordenadas cartesianas:
+        x=rc*m.cos(tetaC)
+        y=rc*m.sin(tetaC)
+        
+        #Calculo coordenadas esféricas:
+        re=m.sqrt(m.pow(x,2)+m.pow(y,2)+m.pow(z,2))
+        tetaE=m.atan(y/x)
+        tetaE=(tetaE*180)/m.pi
+        fi=m.acos(z/re)
+        fi=(fi*180)/m.pi
+
+        print("""
+Coordenadas Cartesianas:
+x=%s
+y=%s
+z=%s
+
+Coordenadas Esféricas:
+r=%s
+teta=%s
+fi=%s
+        """%(x,y,z,re,tetaE,fi))
+    else:
+        print("Error, no ingreso tres coordenadas.")
+elif calcop.startswith("Esfer",0,5) or calcop.startswith("esfer",0,5):
+    calcop=calcop.replace("Esfer ","")
+    calcop=calcop.replace("esfer ","")
+
+    esfer=calcop.split(" ")
+
+    re, tetaE, fi = float(esfer[0]), float(esfer[1]), float(esfer[2])
+
+    if len(esfer)==3:
+        #Calculo coordenadas cartesianas:
+        x=re*m.sin(fi)*m.cos(tetaE)
+        y=rc*m.sin(fi)*m*m.sin(tetaE)
+        z=rc*m.cos(fi)
+        
+        #Calculo coordenadas cilindricas:
+        rc=m.sqrt(m.pow(x,2)+m.pow(y,2))
+        tetaC=m.atan(y/x)
+        tetaC=(tetaC*180)/m.pi
+
+        print("""
+Coordenadas Cartesianas:
+x=%s
+y=%s
+z=%s
+
+Coordenadas Cilindricas:
+r=%s
+teta=%s
+z=%s
+        """%(x,y,z,rc,tetaC,z))
+    else:
+        print("Error, no ingreso tres coordenadas.")
+else:
+    print("Error de entrada, no se reconoció el comando.")
 
 px,py,pz=0,1,2
 P=[[px],[py],[pz]]
